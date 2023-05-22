0adf7c288d509be59d6918562c764b66dafd840e
Kevin Alila <85939629+Alila-kevin@users.noreply.github.com>
2023-05-22 17:54:01 +0300

    Update index.php
    keep

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>simple calculator</title>
+	<title>calculator</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
